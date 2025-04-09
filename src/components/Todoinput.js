b54fd39521bd8ada6829b98d0c5fc1033dc43c8e
divyasuru <surusaidivya@gmail.com>
2025-04-09 11:32:45 +0530

    edit

--- src/components/Todoinput.js
+++ src/components/Todoinput.js
@@ -1,31 +1,41 @@
 import React, { useState } from 'react'
 import List from './List'
 
-const Todoinput = ({placeholder}) => {
-  const [inputvalue,setinputvalue] = useState('');
-  const [task,settask] = useState([]);
+const Todoinput = ({ placeholder }) => {
+  const [inputvalue, setinputvalue] = useState('');
+  const [task, settask] = useState([]);
+  const [editIndex, setEditIndex] = useState(null);
 
-  const handleSubmit = () =>{    
-      settask([...task,inputvalue]);
-      setinputvalue('');    
-  }
+  const handleSubmit = () => {
+    if (inputvalue.trim() !== '') {
+      if (editIndex !== null) {
+        const editedTasks = [...task];
+        editedTasks[editIndex] = inputvalue;
+        settask(editedTasks);
+        setEditIndex(null); 
+      } else {
+        settask([...task, inputvalue]);
+      }
+      setinputvalue('');
+    }
+  };
 
   const handleDelete = (deleteIndex) => {
-    const updatedtasks = task.filter((_,index) => index !== deleteIndex);
+    const updatedtasks = task.filter((_, index) => index !== deleteIndex);
     settask(updatedtasks);
   }
 
-  const handleEdit = (editindex) => {
-    const edittask = task.find(index =>index === editindex );
-    
-  }
+  const handleEdit = (index) => {
+    setinputvalue(task[index]);
+    setEditIndex(index);        
+  };
   return (
     <>
-    <div className='input-container'>
-        <input type='text' placeholder={placeholder} value={inputvalue} onChange={(e) => setinputvalue(e.target.value)}/>
+      <div className='input-container'>
+        <input type='text' placeholder={placeholder} value={inputvalue} onChange={(e) => setinputvalue(e.target.value)} />
         <button type="button" className='input-submit' onClick={handleSubmit}>Submit</button>
-    </div>
-    <List items={task} onDelete={handleDelete} onEdit={handleEdit}/>
+      </div>
+      <List items={task} onDelete={handleDelete} onEdit={handleEdit} />
     </>
   )
 }
